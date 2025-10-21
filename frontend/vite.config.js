e9c8423004e1d044fa4830644f77ad519e1788cc
10thPrince <ntwariprince5050@gmail.com>
2025-10-21 13:55:28 +0200

    resolved err

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // import flowbiteReact from "flowbite-react/plugin/vite";
 
-const api = axios.create({
+const api = {
   baseURL: import.meta.VITE_API_URL,
-});
+}
 
 
 // https://vite.dev/config/
