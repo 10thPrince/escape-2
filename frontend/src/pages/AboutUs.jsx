852a418f4f982894cb3f8e1f07d8e3eb0348aad8
10thPrince <ntwariprince5050@gmail.com>
2025-12-09 22:42:31 +0200

    Change

--- frontend/src/pages/AboutUs.jsx
+++ frontend/src/pages/AboutUs.jsx
@@ -12,6 +12,7 @@ import Footer from '../components/Footer';
 import FloatingSocials from '../components/FloatingSocials';
 
 
+
 const fadeInUp = {
     hidden: { opacity: 0, y: 40 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
