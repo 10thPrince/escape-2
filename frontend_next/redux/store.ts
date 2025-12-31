e7eb26720923420a4c750a5269b0a4e9e917b994
10thPrince <ntwariprince5050@gmail.com>
2025-12-31 18:31:04 +0200

    Added the Projects Fetch successful

--- frontend_next/redux/store.ts
+++ frontend_next/redux/store.ts
@@ -1,11 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
-import authSlice from './slices/authSlice'
+import { configureStore } from '@reduxjs/toolkit'
+import { apiSlice } from './api/apiSlice'
+import authReducer from './slices/authSlice'
+import projectsReducer from './api/projectsSlice'
 
 export const store = configureStore({
-    reducer: {
-        auth: authSlice
-    },
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+    projects: projectsReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 })
+
 
 export type RootState = ReturnType<typeof store.getState>
 
