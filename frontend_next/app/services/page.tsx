849f6879f9ecae3ebaccaefaf2da86443f7fdab4
10thPrince <ntwariprince5050@gmail.com>
2026-01-08 16:59:32 +0200

    service page changes

---
+++ frontend_next/app/services/page.tsx
@@ -0,0 +1,12 @@
+import ServicesPreview from '@/components/ServicesPreview'
+import React from 'react'
+
+const ServicesPage = () => {
+  return (
+    <div>
+        <ServicesPreview />
+    </div>
+  )
+}
+
+export default ServicesPage
